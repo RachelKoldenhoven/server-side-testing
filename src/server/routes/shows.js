72690b30a93475a03bfea04801b020a8f6a64cc1
RachelKoldenhoven <rachel@koldenfrozen.com>
2016-03-08 14:29:48 -0700

    four tests passing

--- src/server/routes/shows.js
+++ src/server/routes/shows.js
@@ -7,7 +7,31 @@ function Shows() {
 };
 
 router.get('/shows', function(req, res, next) {
-  res.status(200).json('testing');
+  Shows().select().then(function(result) {
+    res.status(200).json(result);
+  })
+});
+
+router.get('/shows/:id', function(req, res, next) {
+  Shows().select().where('id', req.params.id).then(function(result) {
+    res.status(200).json(result);
+  })
+});
+
+
+router.post('/shows', function(req, res, next) {
+  var newShow = req.body;
+  Shows().insert({ name: newShow.name, channel: newShow.channel, genre: newShow.genre, rating: newShow.rating, explicit: newShow.explicit}).returning('id').then(function(result) {
+    res.status(200).json(result);
+  })
+});
+
+
+router.post('/shows/:id', function(req, res, next) {
+  Shows().where('id', req.params.id).update(req.body).returning('id').then(function(result) {
+    console.log('hello');
+    res.status(200).json(result);
+  })
 });
 
 module.exports = router;
--- test/apiTest.js
+++ test/apiTest.js
@@ -12,11 +12,11 @@ describe('API routes', function() {
     beforeEach(function(done) {
         knex.migrate.rollback().then(function() {
             knex.migrate.latest()
-            .then(function() {
-                return knex.seed.run().then(function() {
-                    done()
-                });
-            });
+              .then(function() {
+                  return knex.seed.run().then(function() {
+                      done()
+                  });
+              });
         });
     });
 
@@ -30,27 +30,132 @@ describe('API routes', function() {
 
         it('should get all shows', function(done) {
             chai.request(server)
-            .get('/api/shows')
-            .end(function(err, res) {
-                res.should.have.status(200);
-                res.should.be.json;
-                res.body.should.be.a('array');
-                res.body.length.should.equal(4);
-                res.body[0].should.have.property('name');
-                res.body[0].name.should.equal('Suits');
-                res.body[0].should.have.property('channel');
-                res.body[0].channel.should.equal('USA Network');
-                res.body[0].should.have.property('genre');
-                res.body[0].genre.should.equal('Drama');
-                res.body[0].should.have.property('rating');
-                res.body[0].rating.should.equal(3);
-                res.body[0].should.have.property('explicit');
-                res.body[0].explicit.should.equal(false);
-                done();
-            });
+              .get('/api/shows')
+              .end(function(err, res) {
+                  res.should.have.status(200);
+                  res.should.be.json;
+                  res.body.should.be.a('array');
+                  res.body.length.should.equal(4);
+                  res.body[0].should.have.property('name');
+                  res.body[0].name.should.equal('Suits');
+                  res.body[0].should.have.property('channel');
+                  res.body[0].channel.should.equal('USA Network');
+                  res.body[0].should.have.property('genre');
+                  res.body[0].genre.should.equal('Drama');
+                  res.body[0].should.have.property('rating');
+                  res.body[0].rating.should.equal(3);
+                  res.body[0].should.have.property('explicit');
+                  res.body[0].explicit.should.equal(false);
+                  done();
+              });
         });
 
     });
+
+
+    describe('Get one show', function() {
+
+        it('should get one show', function(done) {
+            chai.request(server)
+              .get('/api/shows/1')
+              .end(function(err, res) {
+                  res.should.have.status(200);
+                  res.should.be.json;
+                  res.body.should.be.a('array');
+                  res.body.length.should.equal(1);
+                  res.body[0].should.have.property('name');
+                  res.body[0].name.should.equal('Suits');
+                  res.body[0].should.have.property('channel');
+                  res.body[0].channel.should.equal('USA Network');
+                  res.body[0].should.have.property('genre');
+                  res.body[0].genre.should.equal('Drama');
+                  res.body[0].should.have.property('rating');
+                  res.body[0].rating.should.equal(3);
+                  res.body[0].should.have.property('explicit');
+                  res.body[0].explicit.should.equal(false);
+                  done();
+              });
+        });
+
+    });
+
+    describe('Add a single show', function() {
+
+        it('should add a single show', function(done) {
+            chai.request(server)
+              .post('/api/shows')
+              .send({
+                  name : 'testing',
+                  channel : 'whatever',
+                  genre : 'great',
+                  rating : 1,
+                  explicit : false
+              })
+              .end(function(err, res) {
+                  console.log(res.body);
+
+                  chai.request(server)
+                    .get('api/shows/' + res.body[0])
+                    .end(function(error, response) {
+                        res.should.have.status(200);
+                        res.should.be.json;
+                        res.body.should.be.a('array');
+                        res.body.length.should.equal(1);
+                        res.body[0].should.have.property('name');
+                        res.body[0].name.should.equal('testing');
+                        res.body[0].should.have.property('channel');
+                        res.body[0].channel.should.equal('whatever');
+                        res.body[0].should.have.property('genre');
+                        res.body[0].genre.should.equal('great');
+                        res.body[0].should.have.property('rating');
+                        res.body[0].rating.should.equal(1);
+                        res.body[0].should.have.property('explicit');
+                        res.body[0].explicit.should.equal(false);
+                    });
+                  done();
+              })
+        })
+    });
+
+    describe('Update a show', function() {
+
+
+        it('should edit a show', function(done) {
+            chai.request(server)
+              .put('/api/shows/1')
+              .send({
+                  name: 'Edited Suits',
+                  channel : 'New Channel',
+                  genre: 'Drama',
+                  rating: 3,
+                  explicit: false
+              })
+              .end(function(err, res) {
+                  console.log(res.body);
+
+                  chai.request(server)
+                    .get('/api/shows/' + res.body)
+                    .end(function(error, response) {
+                        res.should.have.status(200);
+                        res.should.be.json;
+                        res.body.should.be.a('array');
+                        res.body.length.should.equal(1);
+                        res.body[0].should.have.property('name');
+                        res.body[0].name.should.equal('Edited Suits');
+                        res.body[0].should.have.property('channel');
+                        res.body[0].channel.should.equal('New Channel');
+                        res.body[0].should.have.property('genre');
+                        res.body[0].genre.should.equal('Drama');
+                        res.body[0].should.have.property('rating');
+                        res.body[0].rating.should.equal(3);
+                        res.body[0].should.have.property('explicit');
+                        res.body[0].explicit.should.equal(false);
+                    });
+                  done();
+              });
+        });
+    });
+
 
 });
 
